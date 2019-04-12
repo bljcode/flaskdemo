10e3a14217d48b6e759e3a8913445a7bde6decbe
bljcode <fanling5458@126.com>
2019-04-12 17:42:51 +0800

    logger update

--- logger.py
+++ logger.py
@@ -4,6 +4,19 @@ from SafeFileHandler import TimedRotatingFileHandler as newTRFileHandler
 import logging.handlers
 from config import config
 
+#增加一个判断目录存在与否的函数，原本的得先有目录
+def make_dir(make_dir_path):
+    path = make_dir_path.strip()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+#使用如下
+#log_dir_name = "logs"
+#log_file_name = 'logger-' + time.strftime('%Y-%m-%d', time.localtime(time.time())) + '.log'
+#当前目录上层
+#log_file_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)) + os.sep + log_dir_name
+#make_dir(log_file_folder)
+
 applog = logging.getLogger("app")
 file = config['default'].LOGPATH + 'strategy-gate.log'
 handler = None
